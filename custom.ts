2183dedbc5e75ec59e388d6371a8d4feb3e0aaf5
DyslexicWolf <125255208+DyslexicWolf@users.noreply.github.com>
2024-08-08 13:08:51 +0200

    Update main.blocks, custom.ts

--- custom.ts
+++ custom.ts
@@ -1,73 +1,50 @@
+player.execute("/scriptevent cc:getId " + mobs.queryTarget((mobs.target(MY_AGENT)))[0].uniqueId)
+
 /**
  * Custom blocks
  */
 //% color=190 weight=100 block="CodeCosmos"
 namespace CodeCosmos {
 
-    //level1
-
-    //% block="raap gereedschap op"
-    export function collect_item(): void {
+    //% block="Raap gereedschap op"
+    export function collect_tools(): void {
         let localPlayer = mobs.target(TargetSelectorKind.LocalPlayer);
         let inspectedBlock = agent.inspect(AgentInspection.Block, FORWARD + DOWN);
-        if(inspectedBlock == YELLOW_WOOL){
-            mobs.give(localPlayer, YELLOW_WOOL, 1);
-        }else if(inspectedBlock == RED_WOOL){
-            mobs.give(localPlayer, RED_WOOL, 1);
-        }else if(inspectedBlock == GREEN_WOOL){
-            mobs.give(localPlayer, GREEN_WOOL, 1);
-        }else if(inspectedBlock == BLUE_WOOL){
-            mobs.give(localPlayer, BLUE_WOOL, 1);
+
+        switch(inspectedBlock){
+            case YELLOW_WOOL:
+                mobs.give(localPlayer, YELLOW_WOOL, 1);
+                break;
+            case RED_WOOL:
+                mobs.give(localPlayer, RED_WOOL, 1);
+                break;
+            case GREEN_WOOL:
+                mobs.give(localPlayer, GREEN_WOOL, 1);
+                break;
+            case BLUE_WOOL:
+                mobs.give(localPlayer, BLUE_WOOL, 1);
         }
     }
 
-    //% block="Plant bloem"
-    export function place_flower(): void {
-        agent.setSlot(1);
-        agent.place(DOWN);
+    //% block="Raap pickaxe en diamant op"
+    export function collect_pickaxe_and_diamond(): void {
+        let localPlayer = mobs.target(TargetSelectorKind.LocalPlayer);
+        let inspectedBlock = agent.inspect(AgentInspection.Block, FORWARD + DOWN);
+
+        if (inspectedBlock == PURPLE_WOOL) {
+            mobs.give(localPlayer, DIAMOND, 1);
+            mobs.give(localPlayer, IRON_PICKAXE, 1);
+        }
     }
-
-    //% block="Zet klaar"
-    export function setup_1(): void {
-        agent.setItem(BLUE_ORCHID, 64, 1)
-        agent.teleport(world(-73, 69, 267), SOUTH)
-        blocks.fill(AIR, world(247, 74, 43), world(222, 74, 43), FillOperation.Replace)
-    }
-
-
-    //level2
 
     //% block="is blok %block=block onder agent"
     //% block.shadow=minecraftBlock
     export function test_for_block_under_agent(block: number): boolean {
         return blocks.testForBlock(block, agent.getPosition().add(world(0, -1, 0)))
     }
-
-    //% block="Plant boom"
-    export function place_tree(): void {
-        agent.setSlot(1);
-        agent.place(DOWN);
-    }
-
-    //% block="Plant struik"
-    export function place_bush(): void {
-        agent.setSlot(2);
-        agent.place(DOWN);
-    }
-
-    //% block="Zet klaar"
-    export function setup_2(): void {
-        agent.setItem(JUNGLE_SAPLING, 64, 1)
-        agent.setItem(SWEET_BERRIES, 64, 2)
-        agent.teleport(world(-111, 69, 272), SOUTH)
-        blocks.fill(AIR, world(247, 74, 43), world(222, 74, 43), FillOperation.Replace)
-    }
-
-
-    //level3
-
-    //% block="Bamboo naast agent"
-    export function next_to_bamboo(): boolean {
+    
+    //% block="Bamboo plant naast agent"
+    export function next_to_bamboo_plant(): boolean {
         let inspectedLeftBlock = agent.inspect(AgentInspection.Block, LEFT);
         let inspectedRightBlock = agent.inspect(AgentInspection.Block, RIGHT);
         if(inspectedLeftBlock == BAMBOO || inspectedRightBlock == BAMBOO){
@@ -76,31 +53,6 @@ namespace CodeCosmos {
             return false;
         }
     }
-
-    //% block="Spawn panda"
-    export function spawn_panda(): void{
-        agent.setSlot(1);
-        agent.place(DOWN);
-    }
-
-    //% block="Plaats cobblestone"
-    export function place_cobblestone(): void {
-        agent.destroy(DOWN);
-        agent.setSlot(3);
-        agent.place(DOWN);
-    }
-
-    //% block="Zet klaar"
-    export function setup_3(): void {
-        agent.setItem(SPAWN_PANDA, 64, 1)
-        agent.setItem(SWEET_BERRIES, 64, 2)
-        agent.setItem(COBBLESTONE, 64, 3)
-        agent.teleport(world(-133, 68, 265), SOUTH)
-        blocks.fill(AIR, world(247, 74, 43), world(222, 74, 43), FillOperation.Replace)
-    }
-
-
-    //level4
 
     //% block="Struik naast agent"
     export function next_to_bush(): boolean {
@@ -124,21 +76,9 @@ namespace CodeCosmos {
         }
     }
 
-    //% block="Spawn ocelot"
-    export function spawn_ocelot(): void {
-        agent.setSlot(1)
-        agent.place(DOWN);
-    }
-
-    //% block="Spawn papegaai"
-    export function spawn_parrot(): void {
-        agent.setSlot(2)
-        agent.place(DOWN);
-    }
-
     //% block="Hekken voor agent"
     export function fence_infrontof_agent(): boolean {
-        let inspectedBlock = agent.inspect(AgentInspection.Block, FORWARD)
+        let inspectedBlock = agent.inspect(AgentInspection.Block, FORWARD);
         if(inspectedBlock == JUNGLE_FENCE){
             return true;
         }else{
@@ -148,7 +88,7 @@ namespace CodeCosmos {
 
     //% block="Fakkel voor agent"
     export function torch_infrontof_agent(): boolean {
-        let inspectedBlock = agent.inspect(AgentInspection.Block, FORWARD)
+        let inspectedBlock = agent.inspect(AgentInspection.Block, FORWARD);
         if (inspectedBlock == TORCH) {
             return true;
         } else {
@@ -156,16 +96,94 @@ namespace CodeCosmos {
         }
     }
 
-    //% block="Zet klaar"
-    export function setup_4(): void {
-        agent.setItem(SPAWN_OCELOT, 64, 1)
-        agent.setItem(SPAWN_PARROT, 64, 2)
-        agent.teleport(world(-133, 68, 265), SOUTH)
-        blocks.fill(AIR, world(247, 74, 43), world(222, 74, 43), FillOperation.Replace)
+    //% block="Spawn ocelot"
+    export function spawn_ocelot(): void {
+        agent.setSlot(1);
+        agent.place(DOWN);
     }
 
+    //% block="Spawn papegaai"
+    export function spawn_parrot(): void {
+        agent.setSlot(2);
+        agent.place(DOWN);
+    }
 
-    //level5
+    //% block="Spawn panda"
+    export function spawn_panda(): void {
+        agent.setSlot(1);
+        agent.place(DOWN);
+    }
 
+    //% block="Plant bloem"
+    export function place_flower(): void {
+        agent.setSlot(1);
+        agent.place(DOWN);
+    }
 
+    //% block="Plant boom"
+    export function place_tree(): void {
+        agent.setSlot(1);
+        agent.place(DOWN);
+    }
+
+    //% block="Plant struik"
+    export function place_bush(): void {
+        agent.setSlot(2);
+        agent.place(DOWN);
+    }
+
+    //% block="Plaats cobblestone"
+    export function place_cobblestone(): void {
+        agent.destroy(DOWN);
+        agent.setSlot(3);
+        agent.place(DOWN);
+    }
+
+    //% block="Plaats grasblok"
+    export function place_grassblock(): void {
+        agent.destroy(DOWN);
+        agent.setSlot(3);
+        agent.place(DOWN);
+    }
+
+    //% block="Zet klaar"
+    export function setup_1(): void {
+        agent.setItem(BLUE_ORCHID, 64, 1);
+        agent.teleport(world(-73, 69, 267), SOUTH);
+        blocks.fill(AIR, world(247, 74, 43), world(222, 74, 43), FillOperation.Replace);
+    }
+
+    //% block="Zet klaar"
+    export function setup_2(): void {
+        agent.setItem(JUNGLE_SAPLING, 64, 1);
+        agent.setItem(SWEET_BERRIES, 64, 2);
+        agent.teleport(world(-111, 69, 272), SOUTH);
+        blocks.fill(AIR, world(247, 74, 43), world(222, 74, 43), FillOperation.Replace);
+    }
+
+    //% block="Zet klaar"
+    export function setup_3(): void {
+        agent.setItem(SPAWN_PANDA, 64, 1);
+        agent.setItem(SWEET_BERRIES, 64, 2);
+        agent.setItem(COBBLESTONE, 64, 3);
+        agent.teleport(world(-133, 68, 265), SOUTH);
+        blocks.fill(AIR, world(247, 74, 43), world(222, 74, 43), FillOperation.Replace);
+    }
+
+    //% block="Zet klaar"
+    export function setup_4(): void {
+        agent.setItem(SPAWN_OCELOT, 64, 1);
+        agent.setItem(SPAWN_PARROT, 64, 2);
+        agent.teleport(world(-133, 68, 265), SOUTH);
+        blocks.fill(AIR, world(247, 74, 43), world(222, 74, 43), FillOperation.Replace);
+    }
+
+    //% block="Zet klaar"
+    export function setup_5(): void {
+        agent.setItem(SPAWN_PANDA, 64, 1);
+        agent.setItem(SPAWN_PARROT, 64, 2);
+        agent.setItem(GRASS, 64, 3);
+        agent.teleport(world(-133, 68, 265), SOUTH);
+        blocks.fill(AIR, world(247, 74, 43), world(222, 74, 43), FillOperation.Replace);
+    }
 }
