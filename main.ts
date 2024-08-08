78f01bd492bd070afc1f83c7986382bc8c85c1b8
DyslexicWolf <125255208+DyslexicWolf@users.noreply.github.com>
2024-08-08 15:50:37 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,76 @@
+player.onChat("level2", function () {
+    CodeCosmos.setup_level2()
+    for (let index = 0; index < 6; index++) {
+        agent.move(FORWARD, 1)
+        if (CodeCosmos.test_for_block_under_agent(GRASS) || CodeCosmos.test_for_block_under_agent(PODZOL)) {
+            CodeCosmos.place_tree()
+        } else {
+            CodeCosmos.place_bush()
+        }
+        agent.turn(RIGHT_TURN)
+        agent.move(FORWARD, 1)
+        if (CodeCosmos.test_for_block_under_agent(GRASS) || CodeCosmos.test_for_block_under_agent(PODZOL)) {
+            CodeCosmos.place_tree()
+        } else {
+            CodeCosmos.place_bush()
+        }
+        agent.turn(LEFT_TURN)
+    }
+    agent.turn(RIGHT_TURN)
+    for (let index = 0; index < 5; index++) {
+        agent.move(FORWARD, 1)
+        if (CodeCosmos.test_for_block_under_agent(GRASS) || CodeCosmos.test_for_block_under_agent(PODZOL)) {
+            CodeCosmos.place_tree()
+        } else {
+            CodeCosmos.place_bush()
+        }
+    }
+    for (let index = 0; index < 2; index++) {
+        agent.move(FORWARD, 1)
+        if (CodeCosmos.test_for_block_under_agent(GRASS) || CodeCosmos.test_for_block_under_agent(PODZOL)) {
+            CodeCosmos.place_tree()
+        } else {
+            CodeCosmos.place_bush()
+        }
+        agent.turn(LEFT_TURN)
+        agent.move(FORWARD, 1)
+        if (CodeCosmos.test_for_block_under_agent(GRASS) || CodeCosmos.test_for_block_under_agent(PODZOL)) {
+            CodeCosmos.place_tree()
+        } else {
+            CodeCosmos.place_bush()
+        }
+        agent.turn(RIGHT_TURN)
+    }
+    agent.move(FORWARD, 1)
+    if (CodeCosmos.test_for_block_under_agent(GRASS) || CodeCosmos.test_for_block_under_agent(PODZOL)) {
+        CodeCosmos.place_tree()
+    } else {
+        CodeCosmos.place_bush()
+    }
+    agent.turn(LEFT_TURN)
+    for (let index = 0; index < 5; index++) {
+        agent.move(FORWARD, 1)
+        if (CodeCosmos.test_for_block_under_agent(GRASS) || CodeCosmos.test_for_block_under_agent(PODZOL)) {
+            CodeCosmos.place_tree()
+        } else {
+            CodeCosmos.place_bush()
+        }
+    }
+    agent.turn(RIGHT_TURN)
+    agent.move(FORWARD, 1)
+    if (CodeCosmos.test_for_block_under_agent(GRASS) || CodeCosmos.test_for_block_under_agent(PODZOL)) {
+        CodeCosmos.place_tree()
+    } else {
+        CodeCosmos.place_bush()
+    }
+})
 player.onChat("level1", function () {
-    CodeCosmos.setup_level5()
+    CodeCosmos.setup_level1()
     for (let index = 0; index < 4; index++) {
         agent.move(FORWARD, 2)
         agent.turn(RIGHT_TURN)
         agent.move(FORWARD, 3)
-        CodeCosmos.collect_tools()
+        CodeCosmos.collect_tool()
         agent.move(BACK, 1)
         agent.turn(LEFT_TURN)
     }
@@ -13,7 +79,7 @@ player.onChat("level1", function () {
     agent.move(FORWARD, 5)
     agent.turn(RIGHT_TURN)
     agent.move(FORWARD, 2)
-    CodeCosmos.collect_tools()
+    CodeCosmos.collect_tool()
     agent.move(BACK, 2)
     agent.turn(LEFT_TURN)
     agent.move(FORWARD, 1)
