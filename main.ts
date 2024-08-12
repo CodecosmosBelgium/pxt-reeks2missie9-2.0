62d66ad239071eeba8208dd9bd5b1acb10ff9a9c
DyslexicWolf <125255208+DyslexicWolf@users.noreply.github.com>
2024-08-12 12:00:08 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -45,6 +45,22 @@ player.onChat("level2", function () {
 })
 player.onChat("level4", function () {
     CodeCosmos.setup_level4()
+    for (let index = 0; index < 3; index++) {
+        agent.move(FORWARD, 1)
+        if (CodeCosmos.no_ironblock_under_agent() && CodeCosmos.next_to_tree()) {
+            CodeCosmos.spawn_parrot()
+        } else if (CodeCosmos.torch_infrontof_agent()) {
+            agent.turn(RIGHT_TURN)
+        } else if (CodeCosmos.fence_infrontof_agent()) {
+            agent.turn(LEFT_TURN)
+        } else if (CodeCosmos.test_for_block_under_agent(GRASS) && CodeCosmos.next_to_bush()) {
+            CodeCosmos.spawn_ocelot()
+        } else {
+            for (let index = 0; index < 4; index++) {
+                agent.turn(LEFT_TURN)
+            }
+        }
+    }
     for (let index = 0; index < 4; index++) {
         agent.move(FORWARD, 1)
         if (CodeCosmos.no_ironblock_under_agent() && CodeCosmos.next_to_tree()) {
@@ -61,8 +77,24 @@ player.onChat("level4", function () {
             }
         }
     }
+    for (let index = 0; index < 1; index++) {
+        agent.move(FORWARD, 1)
+        if (CodeCosmos.no_ironblock_under_agent() && CodeCosmos.next_to_tree()) {
+            CodeCosmos.spawn_parrot()
+        } else if (CodeCosmos.torch_infrontof_agent()) {
+            agent.turn(RIGHT_TURN)
+        } else if (CodeCosmos.fence_infrontof_agent()) {
+            agent.turn(LEFT_TURN)
+        } else if (CodeCosmos.test_for_block_under_agent(GRASS) && CodeCosmos.next_to_bush()) {
+            CodeCosmos.spawn_ocelot()
+        } else {
+            for (let index = 0; index < 4; index++) {
+                agent.turn(LEFT_TURN)
+            }
+        }
+    }
     agent.turn(LEFT_TURN)
-    for (let index = 0; index < 7; index++) {
+    for (let index = 0; index < 3; index++) {
         agent.move(FORWARD, 1)
         if (CodeCosmos.no_ironblock_under_agent() && CodeCosmos.next_to_tree()) {
             CodeCosmos.spawn_parrot()
