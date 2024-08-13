030167c2484a012b0b4c6bb665413222011bcc47
DyslexicWolf <125255208+DyslexicWolf@users.noreply.github.com>
2024-08-13 11:07:50 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -45,39 +45,7 @@ player.onChat("level2", function () {
 })
 player.onChat("level4", function () {
     CodeCosmos.setup_level4()
-    for (let index = 0; index < 3; index++) {
-        agent.move(FORWARD, 1)
-        if (CodeCosmos.no_ironblock_under_agent() && CodeCosmos.next_to_tree()) {
-            CodeCosmos.spawn_parrot()
-        } else if (CodeCosmos.torch_infrontof_agent()) {
-            agent.turn(RIGHT_TURN)
-        } else if (CodeCosmos.fence_infrontof_agent()) {
-            agent.turn(LEFT_TURN)
-        } else if (CodeCosmos.test_for_block_under_agent(GRASS) && CodeCosmos.next_to_bush()) {
-            CodeCosmos.spawn_ocelot()
-        } else {
-            for (let index = 0; index < 4; index++) {
-                agent.turn(LEFT_TURN)
-            }
-        }
-    }
-    for (let index = 0; index < 4; index++) {
-        agent.move(FORWARD, 1)
-        if (CodeCosmos.no_ironblock_under_agent() && CodeCosmos.next_to_tree()) {
-            CodeCosmos.spawn_parrot()
-        } else if (CodeCosmos.torch_infrontof_agent()) {
-            agent.turn(RIGHT_TURN)
-        } else if (CodeCosmos.fence_infrontof_agent()) {
-            agent.turn(LEFT_TURN)
-        } else if (CodeCosmos.test_for_block_under_agent(GRASS) && CodeCosmos.next_to_bush()) {
-            CodeCosmos.spawn_ocelot()
-        } else {
-            for (let index = 0; index < 4; index++) {
-                agent.turn(LEFT_TURN)
-            }
-        }
-    }
-    for (let index = 0; index < 1; index++) {
+    for (let index = 0; index < 8; index++) {
         agent.move(FORWARD, 1)
         if (CodeCosmos.no_ironblock_under_agent() && CodeCosmos.next_to_tree()) {
             CodeCosmos.spawn_parrot()
@@ -94,23 +62,7 @@ player.onChat("level4", function () {
         }
     }
     agent.turn(LEFT_TURN)
-    for (let index = 0; index < 3; index++) {
-        agent.move(FORWARD, 1)
-        if (CodeCosmos.no_ironblock_under_agent() && CodeCosmos.next_to_tree()) {
-            CodeCosmos.spawn_parrot()
-        } else if (CodeCosmos.torch_infrontof_agent()) {
-            agent.turn(RIGHT_TURN)
-        } else if (CodeCosmos.fence_infrontof_agent()) {
-            agent.turn(LEFT_TURN)
-        } else if (CodeCosmos.test_for_block_under_agent(GRASS) && CodeCosmos.next_to_bush()) {
-            CodeCosmos.spawn_ocelot()
-        } else {
-            for (let index = 0; index < 4; index++) {
-                agent.turn(LEFT_TURN)
-            }
-        }
-    }
-    for (let index = 0; index < 9; index++) {
+    for (let index = 0; index < 12; index++) {
         agent.move(FORWARD, 1)
         if (CodeCosmos.no_ironblock_under_agent() && CodeCosmos.next_to_tree()) {
             CodeCosmos.spawn_parrot()
