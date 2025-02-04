7990e3d0c45f15701f85ea3eafb7f0df8092f752
Tor Wylin <125255208+DyslexicWolf@users.noreply.github.com>
2025-02-04 13:22:54 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 player.onChat("level2", function () {
     CodeCosmos.setup_level2()
+    CodeCosmos.spawn_panda()
     agent.turn(RIGHT_TURN)
     for (let index = 0; index < 6; index++) {
         agent.move(FORWARD, 1)
